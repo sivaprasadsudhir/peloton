49c74b14a70d4bcf3d07ddd06c1bd45b407bb681
pbollimp <pbollimp@andrew.cmu.edu>
2018-05-13 20:56:01 -0400

    put correct knobs

--- src/include/brain/brain.h
+++ src/include/brain/brain.h
@@ -40,7 +40,7 @@ class BrainEnvironment {
       sql_connection_{
           settings::SettingsManager::GetString(
               settings::SettingId::peloton_address)} {
-                  index_selection_knobs = {3, 3, 10}; }
+                  index_selection_knobs = {3, 2, 10}; }
 
   IndexSelectionKnobs GetIndexSelectionKnobs() { return index_selection_knobs; }
 
