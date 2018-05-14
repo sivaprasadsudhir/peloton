937a4490671839e3d7784055c882c85d64392898
pbollimp <pbollimp@andrew.cmu.edu>
2018-05-13 22:19:31 -0400

    some fixes

--- src/traffic_cop/traffic_cop.cpp
+++ src/traffic_cop/traffic_cop.cpp
@@ -179,7 +179,7 @@ executor::ExecutionResult TrafficCop::ExecuteHelper(
   if (settings::SettingsManager::GetBool(
           settings::SettingId::brain_data_collection)) {
 
-    if(statement_->GetQueryType() != QueryType::QUERY_INSERT){
+    if(statement_ && statement_->GetQueryType() != QueryType::QUERY_INSERT){
       tcop_txn_state_.top().first->AddQueryString(
           statement_->GetQueryString().c_str());
     }
